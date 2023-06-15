efc79888d4b384c8a82aa4ad20d858b881808c12
fares ayman <faresbarakat289@gmail.com>
2023-06-15 16:20:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line using puts function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
+
