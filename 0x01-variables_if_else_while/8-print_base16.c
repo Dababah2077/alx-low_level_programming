6f3a27bde88d1c45a37cf00b9fbce5374538a701
fares ayman <faresbarakat289@gmail.com>
2023-06-16 15:06:06 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 48; m <= 57; m++)
+	{
+		putchar(m);
+	}
+	for (n = 97; n <= 102; m++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
