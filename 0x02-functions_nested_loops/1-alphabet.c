074e4bf5646b2685e1100438ad258680c23ee40a
fares ayman <faresbarakat289@gmail.com>
2023-06-20 16:12:43 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet - Entry point
  * Return: Always 0 (Success)
@@ -7,7 +8,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++;)
-		putchar(i);
-	putchar('\n');
+	for (i = 'a': i <= 'z': i++)
+		-putchar(i);
+	-putchar('\n');
 }
