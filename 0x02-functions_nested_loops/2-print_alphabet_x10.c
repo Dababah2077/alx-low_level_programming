aea498c87c0db9c8dda526fa8a75be537138482d
fares ayman <faresbarakat289@gmail.com>
2023-06-20 16:41:28 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int line, i;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (i = 'a'; i = 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
