5f69c2c00f5705c89a2b72a6c08418c45c6a536b
fares ayman <faresbarakat289@gmail.com>
2023-06-20 15:52:31 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n = 97;
+	int i = 97;
 
-	while (n <= 122)
+	while (i <= 122)
 	{
-		putchar(n);
-		n++;
+		putchar(i);
+		i++;
 	}
 putchar('\n');
 return (0);
