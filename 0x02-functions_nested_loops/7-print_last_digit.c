dd91b243ec6f73f8058933bac998dac15f105f31
fares ayman <faresbarakat289@gmail.com>
2023-06-20 18:36:54 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * print_last_digit: prints last digit of a number
+ * @n: takes number input
+ * Return: lastdigit
+ */
+int print_last_digit(int n)
+{
+	int lastdigit
+	if (n < 0)
+		lastdigit = -1 * (n % 10);
+	else
+		lastdigit = n % 10;
+
+	_putchar(lastdigit +'0');
+	return (lastdigit);
+}
