f9551adc0297aad0c2a40973734e2aec2150638c
fares ayman <faresbarakat289@gmail.com>
2023-06-20 17:07:10 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * islower - Entry point
+ * Return: 1 if 'c' is lowercase
+ * otherwise Always 0 (Success)
+ */
+int _islower(int c);
+{
+	int c;
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
