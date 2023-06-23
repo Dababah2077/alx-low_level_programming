a1559a2be373c3559c3305059fa539cb4a4979f6
fares ayman <faresbarakat289@gmail.com>
2023-06-23 05:34:54 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Entry point
+ * mul: multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: return results
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
