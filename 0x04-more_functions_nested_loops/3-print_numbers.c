fa07bee066657057640fef192962e190091be168
fares ayman <faresbarakat289@gmail.com>
2023-06-23 06:11:55 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_numbers(void)
 {
-	int n = 48;
+	int n;
 
-	for (n >= 48; n <= 57; n++;)
+	while (n <= 48 && n >= 57)
 	{
-		_putchar (n);
+		_putchar(n);
+		n++;
 	}
-	_putchar ('\n');
-	return (0);
+	_putchar('\n');
 }
