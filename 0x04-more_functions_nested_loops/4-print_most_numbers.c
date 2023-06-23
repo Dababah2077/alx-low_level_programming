e551f6c0f13772b8e18d62fef46ba504ecf2645b
fares ayman <faresbarakat289@gmail.com>
2023-06-23 08:12:30 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,18 +3,19 @@
 /**
  * print_most_numbers - Entry point
  * print_most_numbers: prints numbers from 0 to 9
- * Return: void
+ * Return: Always 0 (Success)
  */
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	while (c >= 48 && c <= 57)
+	for (c = 0; c <= 9; c++)
 	{
-		if (c == 50 || c == 52)
+		if (c == 2 || c == 4)
 		{
 			continue;
 		}
-		_putchar(c);
+		_putchar(num + 48);
 	}
+	_putchar('\n');
 }
