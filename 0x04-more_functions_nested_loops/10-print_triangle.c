687bc92384d9e8d4dfd4656da65e7211a4412493
fares ayman <faresbarakat289@gmail.com>
2023-06-23 13:59:57 -0700

    10-triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_triangle(int size)
 {
-	int x, y;
+	int x, y, z;
 
 	if (size <= 0)
 	{
@@ -15,9 +15,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-	for (x = 48; x < size; x++)
+	for (x = 0; x < size; x++)
 	{
 	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
 	{
 	_putchar(35);
 	}
