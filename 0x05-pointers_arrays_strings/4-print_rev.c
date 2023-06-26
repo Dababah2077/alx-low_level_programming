9a41828ccba2990f56ddaf3da4cd7ac06d4c43f6
fares ayman <faresbarakat289@gmail.com>
2023-06-26 16:49:12 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * print_rev: prints a string, in reverse, followed by a new line
+ * @s: checks input of function
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	char str;
+
+	str =//bug strrev(s);
+	printf("%d", str);
+}
