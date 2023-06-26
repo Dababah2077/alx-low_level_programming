9efeaaf65109ba6dfc3620c8d57d6e867321dceb
fares ayman <faresbarakat289@gmail.com>
2023-06-26 15:40:07 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * _strlen: returns the length of a string
+ * @s: checks input of function
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int str;
+
+	str = *s;
+	strlen(str);
+}
