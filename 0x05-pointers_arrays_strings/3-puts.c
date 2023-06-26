c594f190613ef5e479f1ef94299109d305fb0aea
fares ayman <faresbarakat289@gmail.com>
2023-06-26 16:02:16 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - Entry point
+ * _puts:  prints a string, followed by a new line
+ * @str: check input of function
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	printf(str);
+}
