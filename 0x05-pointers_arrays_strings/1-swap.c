174d71ab59b0a24a42b0ed31fd0105ee7e46a420
fares ayman <faresbarakat289@gmail.com>
2023-06-26 13:29:28 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - Entry point
+ * swap_int:  swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int first, second;
+
+	first = *a
+	second = *b
+
+	printf("a = %d", second)
+	printf("b = %d", first)
+}
