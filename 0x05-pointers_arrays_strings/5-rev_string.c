2bf5184712e174fc841824fda8df95f9fc70b8cc
fares ayman <faresbarakat289@gmail.com>
2023-06-26 17:18:07 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rev_string - Entry point
+ * rev_string: a function that reverses a string
+ * @s: checks input of function
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	char *strrev(char *s);
+}
