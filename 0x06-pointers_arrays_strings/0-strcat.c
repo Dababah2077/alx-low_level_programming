820419ef136167bda81e049b2f7ebcf2e167a4b4
fares ayman <faresbarakat289@gmail.com>
2023-06-29 08:37:30 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - Entry point
+ * *_strcat: appends the src string to the dest string
+ * overwriting the terminating null byte (\0) at the end of dest
+ * and then adds a terminating null byte
+ * @src: source file
+ * @dest: destination file
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	char str1, str2;
+
+	src = *str1;
+	dest = *str2;
+
+	_strcat(char *dest, char *src);
+
+	puts(src);
+	puts(dest);
+
+	return (0);
+}
