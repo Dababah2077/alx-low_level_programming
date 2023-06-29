a4a9953bfad9dcb6145319fd762bf6aefc36621a
fares ayman <faresbarakat289@gmail.com>
2023-06-29 12:00:56 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Entry point
+ * _strcmp: compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if true, 0 if false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	if (*s1 != *s2)
+	{
+		result = ((int)*s1 - 0) - ((int)*s2 - 0);
+	}
+	else
+	{
+		result = 0;
+	}
+
+	return (result);
+}
