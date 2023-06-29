146b28c98ca312c1c4c5bcc78ff3abb2e72e9915
fares ayman <faresbarakat289@gmail.com>
2023-06-29 12:28:51 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result;
-
-	while (*s1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			result = ((int)*s1 - 0) - ((int)*s2 - 0);
-			break;
-		}
+		s1++;
+		s2++;
 	}
-	return (result);
+	return (*s1 - *s2);
 }
