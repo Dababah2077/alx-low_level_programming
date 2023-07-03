0decc6c2dec13db2caa81ee0f6c48aaab3241006
fares ayman <faresbarakat289@gmail.com>
2023-07-03 15:27:54 -0700

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_chessboard - Entry point
+ * print_chessboard: prints the chessboard
+ * @a: array of chessboard
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 0; x++)
+	{
+		for (y = 0; y < 0; y++)
+		{
+			_putchar(a[x][y]);
+		}
+	}
+}
