e16e63b2a896e82f6a6432234ae4c8059728dd4a
fares ayman <faresbarakat289@gmail.com>
2023-07-05 09:18:55 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion - Entry point
+ * _puts_recursion: prints a string, followed by a new line
+ * @s: input
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
