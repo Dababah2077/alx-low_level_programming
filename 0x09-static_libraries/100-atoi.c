8bd80d5952d66a74e17f54b8641c32d5de13cd68
fares ayman <faresbarakat289@gmail.com>
2023-07-07 14:30:55 -0700

    0x0A. C - argc, argv

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _atoi - Entry point
+ * _atoi: a program 
+ * @s: input 
+ * Return: Nothing
+ */
+int _atoi(char *s)
+{
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - Entry point
+ * _puts: prints a string, followed by a new line
+ * @str: input of function
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
