11187d0efae7a9d557e8304899c4bfb357240847
fares ayman <faresbarakat289@gmail.com>
2023-07-11 00:34:34 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,36 +27,26 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
+	int size1, size2, x;
 	char *m;
 
 	if (s1 == NULL)
-	{
-		return (NULL);
-	}
+		s2 = "\0";
 	if (s2 == NULL)
-	{
-		return (NULL);
-	}
+		s1 = "\0";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
-	{
 		return (0);
-	}
-	for (i = 0; i < size1 + size2; i++)
+	for (x = 0; x <= size1 + size2; x++)
 	{
-		if (i < size1)
-		{
-			m[i] = s1[i];
-		}
+		if (x < size1)
+			m[x] = s1[x];
 		else
-		{
-			m[i] = s2[i - size1];
-		}
+			m[x] = s2[x - size1];
 	}
-	m[i] = '\0';
+	m[x] = '\0';
 	return (m);
 }
