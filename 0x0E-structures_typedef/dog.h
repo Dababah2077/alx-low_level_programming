3283450d5da43ef331e3d6ad4d9a418dc722b294
fares ayman <faresbarakat289@gmail.com>
2023-07-18 14:36:42 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point
+ * struct dog: poppy
+ * @age: input
+ * @name: input
+ * @owner: input
+ * Return: Always 0 (Success)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
