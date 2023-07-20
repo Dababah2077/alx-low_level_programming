c34a51de61e40b771583f73b37640cd118c4fef8
fares ayman <faresbarakat289@gmail.com>
2023-07-20 04:59:45 -0700

    calc

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -51,7 +51,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -68,7 +68,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a % b);
 }
