5479f61e0b8f737e2b59f074246d9066495f4633
fares ayman <faresbarakat289@gmail.com>
2023-07-21 02:14:05 -0700

    b

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (x = 0; x < n; x++)
 	{
 		printf("%d", va_arg(args, int));
-		if (x != (n-1) && separator != NULL)
+		if (x != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
