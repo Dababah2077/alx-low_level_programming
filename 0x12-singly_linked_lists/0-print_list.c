b0432f96db8c5722f5eb699803186a5f1adef1ae
Dababah2077 <faresbarakat289@gmail.com>
2023-07-27 09:48:15 -0700

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _strlen - Entry point
+ * _strlen: ...
+ * @s: ...
+ * Return: ...
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	if (s == NULL)
+		return (0);
+	while(*s++)
+		x++;
+	return (x);
+}
+
+/**
+ * print_list - Entry point
+ * print_list: ...
+ * @h: ...
+ * Return: ...
+ */
+size_t print_list(const list_t *h)
+{
+	size_t y = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		y++;
+	}
+	return (y);
+}
