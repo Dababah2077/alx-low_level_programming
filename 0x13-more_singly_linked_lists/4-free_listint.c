e94d7a9852cd1868302f16c9264abdc9a7744e93
Dababah2077 <faresbarakat289@gmail.com>
2023-08-01 09:28:10 -0700

    e

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint - Entry point
+ * free_listint: ...
+ * @head: ..
+ * Return: ...
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
