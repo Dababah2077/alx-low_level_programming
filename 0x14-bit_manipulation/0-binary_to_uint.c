9d4b0d9f76268ddfab72ca732ca7c7a7fab06d4c
Dababah2077 <faresbarakat289@gmail.com>
2023-08-03 13:38:05 -0700

    a

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Entry point
+ * binary_to_uint: ...
+ * @b: ...
+ * Return: ...
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		x = x * 2 + (*b++ - '0');
+	}
+	return (x);
+}
